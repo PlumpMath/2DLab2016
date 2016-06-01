82cc6526d4f8e8b965ff1e074d436c53420b29b2
miltonio94 <miltonio94@gmail.com>
2016-06-01 18:51:43 +0100

    one little change that makes a hudge difference
    change the colour of the text inside the tooltip of the pieChart

--- Milton/HTML/gettingStartedWithFinal/js/main.js
+++ Milton/HTML/gettingStartedWithFinal/js/main.js
@@ -12,21 +12,24 @@ function buttClicked(){
     var chart = c3.generate({
         data: {
             columns: formatArray(picColourData.colour, picColourData.percentages),
-            type : "donut",
+            type : "pie",
             onclick: function (d, i) { console.log("onclick", d, i); },
             // onmouseover: function (d, i) { console.log("onmouseover", d, i); },
             // onmouseout: function (d, i) { console.log("onmouseout", d, i); },
             colors: formatColour(picColourData.colour)
         },
-        donut: {
-            title: "The colours"
+        pie: {
+            title: "The colours of the picture",
+            label: {
+                format: function (value, ratio, id) {
+                    return d3.format('$')(value);
+                }
+            }
         }
     });
 
     $("div#pieCharting").empty();
     $("div#pieCharting").append(chart.element);
-    console.log(formatArray(picColourData.colour, picColourData.percentages));
-    console.log("obj?", formatColour(picColourData.colour));
 };
 
 function formatArray(dataA, dataB) {
